82fedf2d116f9306c67ae75536c5c6549f780412
whl961210 <whl961210@gmail.com>
2024-04-12 23:38:50 -0700

    adjusted main accordingly

--- src/main/java/org/dts/Main.java
+++ src/main/java/org/dts/Main.java
@@ -3,11 +3,14 @@ package org.dts;
 import org.dts.model.BinlogEvent;
 import org.dts.model.EventType;
 import org.dts.parser.BinlogParser;
+import org.dts.rabbitmq.EventConsumer;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException {
 
         BinlogParser parser = new BinlogParser("localhost", "testDb", "testCollection");
         BinlogEvent insertEvent = new BinlogEvent(EventType.INSERT, "test", "1", Map.of("name", "John Doe", "age", 30), null);
@@ -19,5 +22,7 @@ public class Main {
         parser.processEvent(updateEvent);
         parser.processEvent(queryEvent);
         parser.processEvent(deleteEvent);
+        EventConsumer consumer = new EventConsumer();
+        consumer.startConsumer();
     }
 }
